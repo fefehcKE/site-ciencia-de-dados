e8b3bdb7c52e1d6c7fdc7f80e111685e633b8508
fernanda fernandes <166623265+fefehcKE@users.noreply.github.com>
2024-11-19 09:07:54 -0300

    Delete site-ciencia-de-dados directory

--- site-ciencia-de-dados/README.md
+++
@@ -1,4 +0,0 @@
-# Projeto Ciência de Dados
-- <a href="https://variavel.com.br/cdtec" target="_blank">Frontpage do Projeto</a>
-
-![Frontpage do Projeto](siteciencia/static/img/site.png)
--- site-ciencia-de-dados/main.py
+++
@@ -1,5 +0,0 @@
-# Certifique-se de estar na pasta raiz do projeto.
-# Crie um ambiente virtual. No terminal, digite: py -3 -m venv venv
-# Ative o ambiente virtual no terminal: venv\Scripts\Activate
-# Instale o flask no termina: pip install flask
-# Ser for preciso, ative novamente o ambiente virtual no terminal: venv\Scripts\Activate
