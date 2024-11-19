931cd997f5318632839dabb8aaf9fa907a080a93
fernanda fernandes <166623265+fefehcKE@users.noreply.github.com>
2024-11-19 09:07:28 -0300

    aoba

---
+++ site-ciencia-de-dados/README.md
@@ -0,0 +1,4 @@
+# Projeto Ciência de Dados
+- <a href="https://variavel.com.br/cdtec" target="_blank">Frontpage do Projeto</a>
+
+![Frontpage do Projeto](siteciencia/static/img/site.png)
---
+++ site-ciencia-de-dados/main.py
@@ -0,0 +1,5 @@
+# Certifique-se de estar na pasta raiz do projeto.
+# Crie um ambiente virtual. No terminal, digite: py -3 -m venv venv
+# Ative o ambiente virtual no terminal: venv\Scripts\Activate
+# Instale o flask no termina: pip install flask
+# Ser for preciso, ative novamente o ambiente virtual no terminal: venv\Scripts\Activate
